3de3193c3a836dbee6f5065909021c3aaf1d3415
bestkoby <58789388+bestkoby@users.noreply.github.com>
2022-05-19 03:58:56 -0400

    DaY2

---
+++ C#Day2/Day2/Day2/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace Day2
+{
+    public class Program
+    {
+         public static void Main(string[] args)
+         {
+             int[] numbers = GenerateNumbers(10);
+             Reverse(numbers);
+             PrintNumbers(numbers);
+         }
+
+         public static int[] GenerateNumbers(int n)
+         {
+             int[] arr = new int[n];
+             for (int i = 1; i <= n; i++)
+                 arr[i] = i;
+             return arr;
+         }
+
+        public static void Reverse(int[] arr)
+        {
+            for (int i = 1; i <= arr.Length / 2; i++)
+            {
+                int temp = arr[i];
+                arr[i] = arr[arr.Length-1-i] ;
+                arr[arr.Length - 1 - i] = temp; 
+            }
+        }
+
+        public static void PrintNumbers(int[] arr)
+        {
+            for (int i = 0; i < arr.Length ; i++)
+            {
+                Console.Write(arr[i]);
+            }
+        }
+
+
+    }
+}
